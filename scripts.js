bd4ffc7323a00846220a7ad14c5b1b13e97c412f
Porscha07 <porscha.uga@gmail.com>
2017-06-05 17:40:29 -0400

    troubleshooting flip render bugs

--- scripts.js
+++ scripts.js
@@ -13,21 +13,21 @@ $(document).ready(function(){
 
 	];
 
-	$('#header-button').css({
-		'font-size':'medium',
-		'font-weight':'bold',
-		'color':'blue',
-		'position': 'relative',
-		'left': '275px'
+	// $('#header-button').css({
+	// 	'font-size':'medium',
+	// 	'font-weight':'bold',
+	// 	'color':'blue',
+	// 	'position': 'relative',
+	// 	'left': '275px'
 		
-	});
-		$('#header-button1').css({
-			'font-size':'medium',
-			'font-weight':'bold',
-			'color':'blue',
-			'position': 'relative',
+	// });
+		// $('#header-button1').css({
+		// 	'font-size':'medium',
+		// 	'font-weight':'bold',
+		// 	'color':'blue',
+		// 	'position': 'relative',
 		// 'right': '-800px'
-	});
+	
 
 	var gridSize = 14;
 	var memoryGameHTML = '';
@@ -53,15 +53,35 @@ $(document).ready(function(){
 				memoryGameHTML +='div class= "card-holder">';
 					memoryGameHTML+= `<div class="card-front">${card}</div>`;
 					memoryGameHTML += `<div class="card-back"></div>`;
-				memoryGameHTML += `</div>`;
-			memoryGameHTML += `</div>`;
+				memoryGameHTML += '</div>';
+			memoryGameHTML += '</div>';
 	}
 	console.log(memoryGameHTML);
 	$('.mg-contents').html(memoryGameHTML);
 
 	$('.card-holder').click(function(){
 		$(this).toggleClass('flip');
-
+		console.log(.cardholder)
 		var cardsUp = $('.flip');
+		console.dir(cardsUp[0])
+		if (cardsUp.length == 2){
+			var card1 = cardsUp[0].children[0].children[0].src;
+			var card2 = cardsUp[0].children[0].children[0].src;
+			// cardsUpImages = cardsUp.find('.card-fron img');
+			if(card1 == card2){
+				cardsUp.removeClass('flip');
+				cardsUp.addClass('matched');
+				var matchedCards = $('matched');
+				if(matchedCards.length == gridSize){
+					alert(" You are officially SMARTER than 57% of America!!!")
+				}
+			}else{
+				alert("Better look next time, click reset to try again!!")
+				setTimeout(() =>{
+					cardsUp.removeClass('flip');
+				},2000);
+			}
+
+		}
 	});
 });
